54b2268b48380200fd3b8b4e981e5acdb01cf2dc
mlbowling <mlbowling@fullsail.edu>
2015-03-12 23:08:19 -0400

    Finished Assignment_Equations
    This is the finished Equation of the Assignment. Please ignore the Code
    in the index.html it is going to be for the final project.

--- Bowling_Michael_Expressions_Assignment/js/script.js
+++ Bowling_Michael_Expressions_Assignment/js/script.js
@@ -1,5 +1,9 @@
-//Expressions - Age Example
+//Assignment Expressions
 
-var yearBorn = 1987;
-var age = 2015 - yearBorn;
-console.log(age);
+var Temp = prompt("Enter tempature at which Originatl Gravity was taken");//user defined value for tempature of wort.
+var OG = prompt("Enter Ogrinal Gravity");//User defined Orignal Gravity
+var FG = prompt("Enter Final Gravity");//User defined Final Gravity
+
+var total = (76.08 * (OG - FG) / (1.775 - OG )) * (FG / 0.794) / Temp;//math equaiption that will esitmate the ABV of a beer.
+var result = "The estimated ABV of your berr is" + total + "% ABV";//defines the result of the equation
+alert(result);//prompt shows the result of the equation which is an estimated ABV of a home brew beer
